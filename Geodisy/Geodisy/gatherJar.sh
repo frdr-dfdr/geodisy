af5b40942602389fda1f2980f8d0fa54ec1a7e92
Paul Dante <paul.dante@ubc.ca>
2021-11-18 01:25:55 -0800

    Fixed gatherJar.sh typo

--- Geodisy/Geodisy/gatherJar.sh
+++ Geodisy/Geodisy/gatherJar.sh
@@ -1,1 +1,1 @@
-cp /home/ubunut/geodisy/Geodisy/Geodisy/target/Geodisy-1.0-SNAPSHOT-jar-with-dependencies.jar /home/ubuntu/geodisy/Geodisy/Geodisy/Geodisy.jar
+cp /home/ubuntu/geodisy/Geodisy/Geodisy/target/Geodisy-1.0-SNAPSHOT-jar-with-dependencies.jar /home/ubuntu/geodisy/Geodisy/Geodisy/Geodisy.jar
