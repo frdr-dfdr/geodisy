a88d2fc880b229abd04068a43d0404ba1fe9b682
Paul Dante <paul.dante@ubc.ca>
2021-11-18 00:44:49 -0800

    Updating gdal/ogr locations for ubuntu env

--- Geodisy/Geodisy/src/main/java/_Strings/ENDPOINT_STRINGS.java
+++ Geodisy/Geodisy/src/main/java/_Strings/ENDPOINT_STRINGS.java
@@ -1,23 +1,23 @@
 package _Strings;
 
 public class ENDPOINT_STRINGS {
-    public static final String GEOSERVER_BASE_DEV       = "http://geoservertest.frdr.ca:8080/geoserver/";
-    public static final String GEOSERVER_BASE_PROD      = "http://prod-gs-g1:8080/geoserver/";
+    public static final String GEOSERVER_BASE_DEV       = "http://test-gs-c1.frdr.ca:8080/geoserver";
+    public static final String GEOSERVER_BASE_PROD      = "http://test-gs-c1.frdr.ca:8080/geoserver";
 
-    public static final String FRONTEND_DEV_ADDRESS     = "https://206-12-95-26.cloud.computecanada.ca";
-    public static final String FRONTEND_PROD_ADDRESS    = "https://geo.frdr-dfdr.ca";
+    public static final String FRONTEND_DEV_ADDRESS     = "https://test-geo-c1.frdr.ca";
+    public static final String FRONTEND_PROD_ADDRESS    = "https://test-geo-c1.frdr.ca";
 
-    public static final String BACKEND_DEV_ADDRESS      = "geoservertest.frdr.ca";
-    public static final String BACKEND_PROD_ADDRESS     = "prod-gs-g1.frdr.ca";
+    public static final String BACKEND_DEV_ADDRESS      = "test-geo-c1.frdr.ca";
+    public static final String BACKEND_PROD_ADDRESS     = "test-geo-c1.frdr.ca";
 
     public static final String DATA_DIR_LOC_LOCAL       = "C:\\geodata\\geoserver\\data\\downloads\\";
-    public static final String DATA_DIR_LOC_CLOUD       = "/geodata/geoserver/data/downloads/";
+    public static final String DATA_DIR_LOC_CLOUD       = "/home/ubuntu/geodisy/Geodisy/Geodisy/data/downloads/";
 
     public static final String WINDOWS_ROOT             = "C:\\Work\\frdr_geodisy\\Geodisy\\Geodisy\\";
     public static final String FRDR_VM_CENTOS_ROOT      = "./";
 
-    public static String DEV_HARVESTER_BASE             = "https://dev3.frdr.ca/harvestapi/";
-    public static String PROD_HARVESTER_BASE            = "https://www.frdr-dfdr.ca/harvestapi/";
+    public static String DEV_HARVESTER_BASE             = "https://dev3-web-c1.frdr.ca/harvestapi/";
+    public static String PROD_HARVESTER_BASE            = "https://dev3-web-c1.frdr.ca/harvestapi/";
 
     //FOR testing on TEST
 
